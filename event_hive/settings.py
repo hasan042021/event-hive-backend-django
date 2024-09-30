c0d7922f38230d5f4acb81c1aa24cda239624ebd
Hasan <hasan.dev04@gmail.com>
2024-09-30 21:57:14 +0600

    origins updated

--- event_hive/settings.py
+++ event_hive/settings.py
@@ -72,20 +72,9 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://event-hive-client-react-102.onrender.com/",
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-]
-# CORS_ORIGIN_ALLOW_ALL = True
-
-# CORS_ORIGIN_WHITELIST = [
-#     "http://localhost:5173",
-#     # other origins...
-# ]
-
-# CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "event_hive.urls"
 
