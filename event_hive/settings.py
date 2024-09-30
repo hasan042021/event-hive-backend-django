c4208ab16160cba2278caa8ee1d502f7bac47184
Hasan <hasan.dev04@gmail.com>
2024-09-30 22:10:31 +0600

    settings updated

--- event_hive/settings.py
+++ event_hive/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "https://event-hive-backend-django.onrender.com",
     "https://*.127.0.0.1",
+    "https://event-hive-client-react-102.onrender.com",
 ]
 
 
@@ -72,7 +73,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://event-hive-client-react-102.onrender.com/",
+    "https://event-hive-client-react-102.onrender.com",
 ]
 
 
