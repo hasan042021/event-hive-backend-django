f2ca81b359d1c9658fe43427a05825361df7986c
Hasan <hasan.dev04@gmail.com>
2024-11-30 16:33:09 +0600

    postgre added

--- event_hive/settings.py
+++ event_hive/settings.py
@@ -110,10 +110,11 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db("DB_URL")
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
