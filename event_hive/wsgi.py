a456a08a09741e5c3f15ce06350c6720ca1a57b8
Hasan <hasan.dev04@gmail.com>
2024-11-30 21:59:35 +0600

    event hive wsgi updated

--- event_hive/wsgi.py
+++ event_hive/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_hive.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "event_hive.settings")
 
 application = get_wsgi_application()
+
+app = application
